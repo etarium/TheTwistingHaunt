4eb5ea61021b631240890ccee9d27bd8cd0d63b0
NoSuchUserException <33333599+NoSuchUserException@users.noreply.github.com>
2017-11-15 01:34:50 -0600

    Added ADT implemetation

--- Combat/src/Encounter.java
+++ Combat/src/Encounter.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
  *
  * @author Jason Richter, Samuel Fiscus
  */
-public class Encounter {
+public class Encounter implements EncounterADT{
     ArrayList<Entity> currentCombatants = new ArrayList<>();
     ArrayList<Entity> rekt = new ArrayList<>();
 }
