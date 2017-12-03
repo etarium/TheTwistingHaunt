162ed54eaf0a8e0f871652b68ded3e0656c95e7d
NoSuchUserException <um1845rr@10.0.1.7>
2017-12-03 15:54:20 -0600

    Worked further on Encounter class, does not run without crashing however. Also edited the Player and entity classes to implement teamIds. Also changed test client to use encounter temporarily

--- Combat/src/TestClient.java
+++ Combat/src/TestClient.java
@@ -1,4 +1,7 @@
 
+import java.util.ArrayList;
+
+
 /**
  *
  * @author Jason Richter, Samuel Fiscus
@@ -10,14 +13,21 @@ public class TestClient {
         StatBlock statBlock1 = new StatBlock(100, 100, 50, 60, 50, 10);
         StatBlock statBlock2 = new StatBlock(100, 100, 50, 60, 50, 10);
 
-        Entity player = new Entity(statBlock1, "Player");
-        Entity opp = new Entity(statBlock2, "Helpless Cripple");
-
+        Entity player = new Entity(statBlock1, "Player", 1);
+        Entity opp = new Entity(statBlock2, "Helpless Cripple", 2);
+        
+        ArrayList<Entity> fighters = new ArrayList<>();
+        fighters.add(opp);
+        fighters.add(player);
+        Encounter fight1 = new Encounter(fighters);
+        fight1.RunCombat();
+        /*
         while ( opp.getStats().isAlive() ) {
             int damage = player.attack(opp);
             player.combatDialog(damage);
             System.out.println();
-
+            
         }//end while
+    */
     }//end main
 }//end class
