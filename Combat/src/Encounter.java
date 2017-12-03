ee93293c2ba34000e852c1b30b7348756b55f505
NoSuchUserException <um1845rr@10.0.1.7>
2017-12-03 15:15:43 -0600

    Added some functionality to Encounter class, still very much WIP

--- Combat/src/Encounter.java
+++ Combat/src/Encounter.java
@@ -1,11 +1,35 @@
 
 import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
 
 /**
- *
  * @author Jason Richter, Samuel Fiscus
  */
-public class Encounter implements EncounterADT{
-    ArrayList<Entity> currentCombatants = new ArrayList<>();
+public class Encounter implements EncounterADT {
+
     ArrayList<Entity> rekt = new ArrayList<>();
+
+    PriorityQueue<Entity> turn = new PriorityQueue<>();
+    private TreeMap<Integer, Entity> entities = new TreeMap<>();
+
+    public Encounter() {
+    }
+
+    public Encounter(ArrayList<Entity> combatants) {
+        for(int i = 0; i > combatants.size(); i++){ //populate tree sorted by initiative
+            entities.put(combatants.get(i).getStats().getInitiative(), combatants.get(i));
+        }
+    }
+
+    public void RunCombat() {
+        boolean valid = runQueue(); //initial enqueue
+        while (valid) {
+
+        }
+    }
+
+    private boolean runQueue() {
+        return true;
+    }
 }
