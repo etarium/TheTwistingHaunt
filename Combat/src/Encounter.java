256300bde3aac951f587260f4b4e64ae740b187c
PermanancyIsDaunting <>
2017-12-03 19:34:32 -0600

    1)TestClient: -added several combatants, each with different initiatives, in order to test that our TreeMap was correctly ordering by initiative -- it is. -changed teamIDs so that all were equal in order to test the combat checker.
    2)Encounter:
    -changed access on data members
    -added an else condition to catch if no conflict present
    -changed firstTeam initialization to assign teamID value and not
    initiative

--- Combat/src/Encounter.java
+++ Combat/src/Encounter.java
@@ -8,9 +8,9 @@ import java.util.TreeMap;
  */
 public class Encounter implements EncounterADT {
 
-    ArrayList<Entity> rekt = new ArrayList<>();
-
-    PriorityQueue<Entity> turn = new PriorityQueue<>();
+    private ArrayList<Entity> rekt = new ArrayList<>();
+    private PriorityQueue<Entity> turn = new PriorityQueue<>();
+    
     private TreeMap<Integer, Entity> entities = new TreeMap<>();
 
     public Encounter() {
@@ -21,25 +21,32 @@ public class Encounter implements EncounterADT {
         for(Entity temp : combatants){
             entities.put(temp.getStats().getInitiative(), temp);
         }
-        System.out.println(entities);
+        //System.out.println(entities); //test print
     }
 
     public void RunCombat() {
         boolean valid = runQueue(); //initial enqueue, never runs combat if failed
         if (valid) {
-            System.out.print("I got this far!");
+            System.out.println("I got this far!");
         }
+        else {
+            System.out.println("No conflict here.");
+        }
+        
+        System.out.println(entities);
+        //all entries currently removed from entities
     }
 
     private boolean runQueue() {
         boolean teamCheck = false; //boolean to check team compositions
         
-        int firstTeam = entities.lastKey();
+        int firstTeam = entities.lastEntry().getValue().getTeamId();
+        System.out.println(firstTeam);
         Entity temp;
         while(!entities.isEmpty()){
             temp = entities.lastEntry().getValue();
             entities.remove(entities.lastKey());
-            System.out.print(temp.getName());
+            System.out.println(temp.getName());
             if(temp.getTeamId() != firstTeam && teamCheck == false)
                 teamCheck = true;
         }
--- Combat/src/TestClient.java
+++ Combat/src/TestClient.java
@@ -13,14 +13,23 @@ public class TestClient {
         //initiative must currently be different
         StatBlock statBlock1 = new StatBlock(100, 100, 50, 60, 50, 15);
         StatBlock statBlock2 = new StatBlock(100, 100, 50, 60, 50, 10);
+        StatBlock statBlock3 = new StatBlock(100, 100, 50, 60, 50, 11);
+        StatBlock statBlock4 = new StatBlock(100, 100, 50, 60, 50, 0);
+        StatBlock statBlock5 = new StatBlock(100, 100, 50, 60, 50, 30);
 
         //stats, team#
-        Entity player = new Entity(statBlock1, "Player", 1);
-        Entity opp = new Entity(statBlock2, "Helpless Cripple", 2);
+        Entity player = new Entity(statBlock1, "Player", 2);
+        Entity opp1 = new Entity(statBlock2, "Helpless Cripple1", 2);
+        Entity opp2 = new Entity(statBlock3, "Helpless Cripple2", 2);
+        Entity opp3 = new Entity(statBlock4, "Helpless Cripple3", 2);
+        Entity opp4 = new Entity(statBlock5, "Helpless Cripple4", 2);
         
         ArrayList<Entity> fighters = new ArrayList<>();
-        fighters.add(opp);
+        fighters.add(opp1);
         fighters.add(player);
+        fighters.add(opp2);
+        fighters.add(opp3);
+        fighters.add(opp4);
         Encounter fight1 = new Encounter(fighters);
         fight1.RunCombat();
         /*
