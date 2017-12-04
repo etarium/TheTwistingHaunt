a5fe9fe0c0e7f8a4af033a134911acae512fe003
NoSuchUserException <jason@Karrick>
2017-12-04 01:41:27 -0600

    Changed Item() and it's extensions to Usable() type for better naming accuracy

---
+++ Combat/src/MultiTargetUsable.java
@@ -0,0 +1,40 @@
+
+/**
+ *
+ * @author Jason Richter, Samuel Fiscus
+ */
+public class MultiTargetUsable extends Usable {
+
+    @Override
+    public void useHP(Entity[] targetList) {
+        for (int i = 0; i < targetList.length; i++) {
+            int currentHP = targetList[i].getStats().getCurrentHealth();
+            int maxHP = targetList[i].getStats().getMaxHealth();
+
+            int newHP = currentHP + potency;
+
+            //if newHP greater than maxHP, target at full health
+            //otherwise, newHP replaces old value
+            int val = (maxHP > newHP) ? newHP : maxHP;
+
+            targetList[i].getStats().setCurrentHealth(val);
+        }
+    }
+
+    /*
+    @Override
+    public void singleTarget(Entity x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void multiTarget(Entity[] group) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void omniTarget(Encounter enc) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+     */
+}
