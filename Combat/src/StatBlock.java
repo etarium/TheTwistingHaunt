4264f1bc71a8baefb705854999b334498ef46833
PermanancyIsDaunting <>
2017-12-05 22:04:56 -0600

    Tiny Update
    Completed javadoc on StatBlock and StatBlockADT

--- Combat/src/StatBlock.java
+++ Combat/src/StatBlock.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author qn4795oh
+ * @author Jason Richter, Samuel Fiscus
  */
-public class StatBlock {
+public class StatBlock implements StatBlockADT{
     
     
     //data members
@@ -30,9 +24,22 @@ public class StatBlock {
     
     //constructors
     
+    /**
+     * Empty constructor for StatBlock.
+     */
     public StatBlock() {
     }
 
+    /**
+     * Filled constructor for StatBlock.
+     * @param currentHealth Integer representing current health
+     * @param maxHealth Integer representing maximum health value
+     * @param specPoints Integer representing current special attack points
+     * @param physDef Integer representing physical defense value
+     * @param evasion Integer representing evasion value
+     * @param physAtt Integer representing physical attack value
+     * @param initiative Integer representing initiative value
+     */
     public StatBlock(int currentHealth, int maxHealth, int specPoints, int physDef, 
                         int evasion, int physAtt, int initiative) {
         this.currentHealth = currentHealth;
@@ -47,76 +54,92 @@ public class StatBlock {
     
     //getters and setters
 
+    @Override
     public int getCurrentHealth() {
         return currentHealth;
     }
 
+    @Override
     public void setCurrentHealth(int currentHealth) {
         this.currentHealth = currentHealth;
     }
 
+    @Override
     public int getMaxHealth() {
         return maxHealth;
     }
 
+    @Override
     public int getSpecPoints() {
         return specPoints;
     }
 
+    @Override
     public void setSpecPoints(int specialPoints) {
         this.specPoints = specialPoints;
     }
     
     
 
+    @Override
     public void setMaxHealth(int maxHealth) {
         this.maxHealth = maxHealth;
     }
 
+    @Override
     public int getPhysDef() {
         return physDef;
     }
 
+    @Override
     public void setPhysDef(int physDef) {
         this.physDef = physDef;
     }
 
+    @Override
     public int getEvasion() {
         return evasion;
     }
 
+    @Override
     public void setEvasion(int evasion) {
         this.evasion = evasion;
     }
 
+    @Override
     public int getPhysAtt() {
         return physAtt;
     }
 
+    @Override
     public void setPhysAtt(int physAtt) {
         this.physAtt = physAtt;
     }
 
+    @Override
     public int getInitiative() {
         return initiative;
     }
 
+    @Override
     public void setInitiative(int initiative) {
         this.initiative = initiative;
     }
     
     //member methods
-    
+    @Override
     public double healthPercentage(){
         return (double)currentHealth/maxHealth;
     }
     
+    @Override
     public boolean isAlive() {
         return currentHealth > 0;
     }
     
+    @Override
     public void decrementSpecPoints(){
         this.setSpecPoints(specPoints - 1);
     }
     
-}
+}//end StatBlock class
