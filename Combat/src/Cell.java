131b16906b0dddabb7416ddebb7d6cb218401057
NoSuchUserException <um1845rr@172.16.69.74>
2018-04-19 16:28:46 -0500

    added full constructor and set/getters

--- Combat/src/Cell.java
+++ Combat/src/Cell.java
@@ -3,10 +3,111 @@
  * @author jason
  */
 public class Cell {
+    private int x;
+    private int y;
+    private int z;
+    private String desc;
+    private String terrain;
+    private String encounter;
+    private String item;
+    private String keyItem;
+    private String reqItem;
+    private boolean north;
+    private boolean south;
+    private boolean east;
+    private boolean west;
 
     public Cell() {
     }
-    //will contain all cell info
+
+    public Cell(int x, int y, int z, String desc, String terrain, String encounter, String item, String keyItem, String reqItem, boolean north, boolean south, boolean east, boolean west) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.desc = desc;
+        this.terrain = terrain;
+        this.encounter = encounter;
+        this.item = item;
+        this.keyItem = keyItem;
+        this.reqItem = reqItem;
+        this.north = north;
+        this.south = south;
+        this.east = east;
+        this.west = west;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getTerrain() {
+        return terrain;
+    }
+
+    public String getEncounter() {
+        return encounter;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public String getKeyItem() {
+        return keyItem;
+    }
+
+    public String getReqItem() {
+        return reqItem;
+    }
+
+    public boolean isNorth() {
+        return north;
+    }
+
+    public boolean isSouth() {
+        return south;
+    }
+
+    public boolean isEast() {
+        return east;
+    }
+
+    public boolean isWest() {
+        return west;
+    }
+
+    public void setEncounter(String encounter) {
+        this.encounter = encounter;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public void setKeyItem(String keyItem) {
+        this.keyItem = keyItem;
+    }
+
+    public void setReqItem(String reqItem) {
+        this.reqItem = reqItem;
+    }
+    
+    
+
+    
+
     
     
 }
