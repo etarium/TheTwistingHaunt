b23791f626ab46949df93aa12c4cbcc040952136
NoSuchUserException <um1845rr@172.16.69.74>
2018-04-19 20:08:26 -0500

    added key item framework

---
+++ Combat/src/KeyItem.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author um1845rr
+ */
+public class KeyItem {
+    
+    private String name;
+    
+}
