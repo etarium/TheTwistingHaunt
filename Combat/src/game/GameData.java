b52e648bbf77ac2b7ec6ec98c5ec5f9c2ff5bc25
PermanancyIsDaunting <samuel.fiscus@gmail.com>
2018-04-22 19:41:27 -0500

    test

--- Combat/src/game/GameData.java
+++ Combat/src/game/GameData.java
@@ -1,6 +1,26 @@
 package game;
 
+import java.util.ArrayList;
+
 public class GameData {
 
-	
+	private Player player;
+    private Cell[][][] cellList;
+    private ArrayList<Usable> usableList;
+    private ArrayList<Equipable> equipList;
+    private ArrayList<KeyItems> keyList;
+    private ArrayList<Encounter> encList;
+    
+    public GameData() {
+    	
+    }
+    
+    public GameData(Player player, Cell[][][] cellList, ArrayList<Usable> usableList,ArrayList<Equipable> equipList,
+    		ArrayList<KeyItems> keyList, ArrayList<Encounter> encList) {
+    	
+    		this.player = player;
+    		this. cellList = cellList;
+    		this.usableList = usableList;
+    		
+    }
 }
