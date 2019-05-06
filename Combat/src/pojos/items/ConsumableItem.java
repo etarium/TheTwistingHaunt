8c46b4785f5535c15b616266de0ed48465517456
Emily Clark <eclark8395@gmail.com>
2019-05-06 15:28:25 -0500

    Updated items

--- Combat/src/pojos/items/ConsumableItem.java
+++ Combat/src/pojos/items/ConsumableItem.java
@@ -1,9 +1,27 @@
 package pojos.items;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class ConsumableItem extends Item{
 	int duration;
 	int numOfTargets;
+	
+	public int getDuration() {
+		return duration;
+	}
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	public int getNumOfTargets() {
+		return numOfTargets;
+	}
+	public void setNumOfTargets(int numOfTargets) {
+		this.numOfTargets = numOfTargets;
+	}
+	
+	@Override
+	public String toString() {
+		return "ConsumableItem [duration=" + duration + ", numOfTargets=" + numOfTargets + ", name=" + name + ", type="
+				+ type + ", description=" + description + ", usedDescription=" + usedDescription + ", stats=" + stats
+				+ ", minLevel=" + minLevel + "]";
+	}
+	
 }
