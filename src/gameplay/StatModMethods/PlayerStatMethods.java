5506b91444de50224388ab744bdef1f12ce4d77f
Emily Clark <eclark8395@gmail.com>
2019-08-30 15:30:38 -0500

    added initial stats

--- src/gameplay/StatModMethods/PlayerStatMethods.java
+++ src/gameplay/StatModMethods/PlayerStatMethods.java
@@ -1,5 +1,6 @@
 package gameplay.StatModMethods;
 
+import pojos.Statblock;
 import uiView.UIMain;
 
 public class PlayerStatMethods {
@@ -16,4 +17,45 @@ public class PlayerStatMethods {
 		UIMain.player.getStats().setCurrentSP(UIMain.player.getStats().getSp());
 		return "Spell Points: " + UIMain.player.getStats().getSp();
 	}
+	
+	public Statblock calculateBaseStats() {
+		//compares the stats of the class and species
+		Statblock stats = new Statblock();
+		stats.setHp(calculateBaseHp());
+		stats.setSp(calculateBaseSp());
+		stats.setAtk(calculateBaseAtk());
+		stats.setDef(calculateBaseDef());
+		
+		return stats;
+	}
+	
+	private int calculateBaseHp() {
+		int hp = UIMain.player.getEntityClass().getStats().getHp() + UIMain.player.getSpeciesObject().getStats().getHp();
+		if(hp > 0) {
+			return hp;
+		}
+		return 0;
+	}
+	private int calculateBaseSp() {
+		int sp = UIMain.player.getEntityClass().getStats().getSp() + UIMain.player.getSpeciesObject().getStats().getSp();
+		if(sp > 0) {
+			return sp;
+		}
+		return 0;
+	}
+	private int calculateBaseAtk() {
+		int atk = UIMain.player.getEntityClass().getStats().getAtk() + UIMain.player.getSpeciesObject().getStats().getAtk();
+		if(atk > 0) {
+			return atk;
+		}
+		return 0;
+	}
+	private int calculateBaseDef() {
+		int def = UIMain.player.getEntityClass().getStats().getDef() + UIMain.player.getSpeciesObject().getStats().getDef();
+		if(def > 0) {
+			return def;
+		}
+		return 0;
+	}
+
 }
