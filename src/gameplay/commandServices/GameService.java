3ab3a9e01e78dcbc30c8f0ff88830b3b6264713b
Emily Clark <clark.emily@mayo.edu>
2019-09-03 14:57:20 -0500

    had an unfinished thought there

--- src/gameplay/commandServices/GameService.java
+++ src/gameplay/commandServices/GameService.java
@@ -26,73 +26,44 @@ public class GameService {
 
 	PlayWindow play;
 	boolean run;
-	
+
 	ObjectMapper mapper = new ObjectMapper();
 	ConfigReader config = new ConfigReader();
-	
+
 	public GameService(PlayWindow play, boolean run) {
 		this.play = play;
 		this.run = run;
-		
+
 	}
-	
+
 	public void quitGame() {
 		run = false;
 		play.exitGame();
 		play = null;
 	}
-	
+
 	public void help() {
 		//this one might not have needed to be factored out, but it gives us a consistent patter to follow
 		new HelpWin();
 	}
-	
+
 	public void saveGame(String parameter) {
 		String rootPath = System.getProperty("user.dir");
 		String saveLoc = config.getProperty("save.location");
-		String encryptionKey = "encrypted1234567";
-		byte[] encryptionKeyBytes = encryptionKey.getBytes();
 		File dir = new File(rootPath+saveLoc+parameter);
 		dir.mkdir();
-	
+
 		try {
-			Cipher cipher = Cipher.getInstance("AES");
-			SecretKey secretKey = new SecretKeySpec(encryptionKeyBytes, "AES");
-		    cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-		    byte[] encryptedPlayer = cipher.doFinal((""+UIMain.player).getBytes());
-		    byte[] encryptedCells = cipher.doFinal((""+UIMain.cells).getBytes());
-			mapper.writeValue(new File(dir+"/player-save.txt"),encryptedPlayer);
-			mapper.writeValue(new File(dir+"/cells-save.txt"), encryptedCells);
+			mapper.writeValue(new File(dir+"/player-save.txt"), UIMain.player);
+			mapper.writeValue(new File(dir+"/cells-save.txt"), UIMain.cells);
 			Logs.LOGGER.info("Successfully saved " + UIMain.player);
-		} catch (Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 			Logs.LOGGER.severe("Unable to save game state,");
 		}
 	}
-	
-	public void loadGame(String selected) {
-		selected = "/"+selected;
-		String rootPath = System.getProperty("user.dir");
-		String saveLoc = config.getProperty("save.location");
-		File dir = new File(rootPath+saveLoc+selected);
-		try {
-			Logs.LOGGER.info("Opening Save " + dir); 
-			
-			File playerFile = new File (dir+"/player-save.txt");
-			File cellsFile = new File (dir+"/cells-save.txt");
-			
-			UIMain.player = mapper.readValue(playerFile,  PlayerEntity.class);
-			UIMain.cells = mapper.readValue(cellsFile, new TypeReference<List<Cell>>(){});
 
-			Logs.LOGGER.info("Loaded player-save from " + selected);
-			Logs.LOGGER.info("Loaded cell-save from " + dir);
-			
-		} catch (IOException e ){
-			Logs.LOGGER.severe("Could not load save " + dir);
-		}
-	}
-	
 	public void options() {
-		
+
 	}
 }
