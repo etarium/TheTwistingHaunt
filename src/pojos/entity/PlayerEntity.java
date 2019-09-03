509c4eb63736d70e9f89abf88abe35cdf5175cd8
Emily Clark <clark.emily@mayo.edu>
2019-09-03 09:40:21 -0500

    added encounter boolean

--- src/pojos/entity/PlayerEntity.java
+++ src/pojos/entity/PlayerEntity.java
@@ -31,89 +31,117 @@ public class PlayerEntity extends Entity{
 	int xpToNextLevel;
 	Location location;
 	public Cell currentCell;
+	public boolean isInEncounter = false;
 	
 	
 	public SpeciesObject getSpeciesObject() {
 		return speciesObject;
 	}
+	
 	public void setSpecies(SpeciesObject speciesObject) {
 		this.speciesObject = speciesObject;
 	}
+	
 	public Location getLocation() {
 		return location;
 	}
+	
 	public void setLocation(Location location) {
 		this.location = location;
 	}
+	
 	public EntityClassObject getEntityClass() {
 		return entityClass;
 	}
+	
 	public void setEntityClass(EntityClassObject entityClass) {
 		this.entityClass = entityClass;
 	}
+	
 	public int getNumOfHands() {
 		return numOfHands;
 	}
+	
 	public void setNumOfHands(int numOfHands) {
 		this.numOfHands = numOfHands;
 	}
+	
 	public Statblock getStats() {
 		return stats;
 	}
+	
 	public void setStats(Statblock stats) {
 		this.stats = stats;
 	}
+	
 	public List<ArmorMaterial> getArmorType() {
 		return armorType;
 	}
+	
 	public void setArmorType(List<ArmorMaterial> list) {
 		this.armorType = list;
 	}
+	
 	public List<WeaponType> getWeaponType() {
 		return weaponType;
 	}
+	
 	public void setWeaponType(List<WeaponType> weaponType) {
 		this.weaponType = weaponType;
 	}
+	
 	public List<Ability> getSkills() {
 		return skills;
 	}
+	
 	public void setSkills(List<Ability> skills) {
 		this.skills = skills;
 	}
+	
 	public List<Quest> getQuests() {
 		return quests;
 	}
+	
 	public void setQuests(List<Quest> quests) {
 		this.quests = quests;
 	}
+	
 	public List<ArmorItem> getEquippedArmor() {
 		return equippedArmor;
 	}
+	
 	public void setEquippedArmor(List<ArmorItem> equippedArmor) {
 		this.equippedArmor = equippedArmor;
 	}
+	
 	public List<WeaponItem> getEquippedWeapons() {
 		return equippedWeapons;
 	}
+	
 	public void setEquippedWeapons(List<WeaponItem> equippedWeapons) {
 		this.equippedWeapons = equippedWeapons;
 	}
+	
 	public Map<Faction, Integer> getFactionLevel() {
 		return factionLevel;
 	}
+	
 	public void setFactionLevel(Map<Faction, Integer> factionLevel) {
 		this.factionLevel = factionLevel;
 	}
+	
 	public int getXp() {
 		return xp;
 	}
+	
 	public void setXp(int xp) {
 		this.xp = xp;
 	}
+	
 	public int getXpToNextLevel() {
 		return xpToNextLevel;
 	}
+	
 	public void setXpToNextLevel(int xpToNextLevel) {
 		this.xpToNextLevel = xpToNextLevel;
 	}
@@ -121,18 +149,24 @@ public class PlayerEntity extends Entity{
 	public Cell getCurrentCell() {
 		return currentCell;
 	}
+	
 	public void setCurrentCell(Cell currentCell) {
 		this.currentCell = currentCell;
 	}
 	
-	@Override
-	public String toString() {
-		return "PlayerEntity [entityClass=" + entityClass + ", species=" + speciesObject + ", numOfHands=" + numOfHands
-				+ ", stats=" + stats + ", armorType=" + armorType + ", weaponType=" + weaponType + ", skills=" + skills
-				+ ", quests=" + quests + ", equippedArmor=" + equippedArmor + ", equippedWeapons=" + equippedWeapons
-				+ ", factionLevel=" + factionLevel + ", xp=" + xp + ", xpToNextLevel=" + xpToNextLevel + ", location="
-				+ location + ", currentCell=" + currentCell + ", name=" + name + ", description=" + description
-				+ ", temperament=" + temperament + ", inventory=" + inventory + ", level=" + level + "]";
+	public void setIsInEncounter(boolean isInEncounter) {
+		this.isInEncounter = isInEncounter;
 	}
 	
+	@Override
+	public String toString() {
+		return "PlayerEntity [entityClass=" + entityClass + ", speciesObject=" + speciesObject + ", numOfHands="
+				+ numOfHands + ", stats=" + stats + ", armorType=" + armorType + ", weaponType=" + weaponType
+				+ ", skills=" + skills + ", quests=" + quests + ", equippedArmor=" + equippedArmor
+				+ ", equippedWeapons=" + equippedWeapons + ", factionLevel=" + factionLevel + ", xp=" + xp
+				+ ", xpToNextLevel=" + xpToNextLevel + ", location=" + location + ", currentCell=" + currentCell
+				+ ", isInEncounter=" + isInEncounter + ", name=" + name + ", description=" + description
+				+ ", temperament=" + temperament + ", species=" + species + ", inventory=" + inventory + ", level="
+				+ level + "]";
+	}
 }
