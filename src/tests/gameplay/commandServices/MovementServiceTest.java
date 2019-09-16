42af9b00ba2abd65967d1eaf1927ae7b9c3c3dfe
Emily Clark <clark.emily@mayo.edu>
2019-09-16 15:13:00 -0500

    initial scaffolding

--- src/tests/gameplay/commandServices/MovementServiceTest.java
+++ src/tests/gameplay/commandServices/MovementServiceTest.java
@@ -1,5 +1,59 @@
 package tests.gameplay.commandServices;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import gameplay.battle.BattleOrder;
+
 public class MovementServiceTest {
 
+	@Mock
+	BattleOrder battleOrder = Mockito.mock(BattleOrder.class);
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@Test
+	public void moveNorthSuccess() {
+		
+	}
+	
+	@Test
+	public void moveNorthFail() {
+		
+	}
+	
+	@Test
+	public void moveSouthSuccess() {
+		
+	}
+	
+	@Test
+	public void moveSouthFail() {
+		
+	}
+	
+	@Test
+	public void moveEastSuccess() {
+		
+	}
+	
+	@Test
+	public void moveEastFail() {
+		
+	}
+	
+	@Test
+	public void moveWestSuccess() {
+		
+	}
+	
+	@Test
+	public void moveWestFail() {
+		
+	}
 }
